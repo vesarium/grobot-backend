f384d77b29565fbe84a8a193c183fc13f40e8c0e
Daniil Murashev <daniil.murashev@gmail.com>
2021-11-08 19:25:44 +0000

    README.md file added

---
+++ src/main/java/com/getitgrown/grobotback/GrobotBackApplication.java
@@ -0,0 +1,13 @@
+package com.getitgrown.grobotback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrobotBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrobotBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getitgrown/grobotback/GrobotBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getitgrown.grobotback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrobotBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
